95ba978d26eb9b09c997b627d0a67541d971f8aa
Leesowon <leesowon00@naver.com>
2020-11-27 02:01:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/babyhealth_recommed2/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.babyhealth_recommed2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
